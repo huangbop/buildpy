7fc82f5ebd31d57cdb2bf71ec688ad26d55be338
Huang Bo <huangbop@gmail.com>
2016-07-30 16:40:56 +0800

    Main

---
+++ src/build.py
@@ -0,0 +1,13 @@
+"""
+M1_Code build file.
+
+Copyright (C) MemoRise Technologies Co., Ltd. 2016-2017. All rights reserved.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
